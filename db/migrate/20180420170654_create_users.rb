34df003b81913c22d3e3c667504606fbeae263c4
Roselou <Elizabethroselou@gmail.com>
2018-04-20 10:30:16 -0700

    initialized app and setup database

---
+++ db/migrate/20180420170654_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :avatar
+      t.string :current_city
+      t.string :password_digest
+      t.string :email
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
